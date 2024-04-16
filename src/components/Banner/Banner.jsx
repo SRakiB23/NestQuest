6615184e406fac54eb19c22adc844a73649c93aa
Rakib <sazzadbasharrakib@Sazzads-MacBook-Pro.local>
2024-04-16 13:49:41 +0600

    changed the slider design a bit

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,64 +1,84 @@
-const Banner = () => {
-  return (
-    <div className="carousel relative md: h-96 pb-4">
-      <div id="slide1" className="carousel-item relative w-full">
-        <img
-          src="https://i.ibb.co/bFSBC4k/banner1.webp"
-          className="w-full object-cover opacity-95"
-        />
-        <div className="absolute inset-0 flex  md:items-center justify-center">
-          <h2 className="animate__animated animate__bounceInDown text-5xl text-orange-500 font-bold md:bg-stone-100 p-2">
-            GET STARTED RIGHT NOW!
-          </h2>
-        </div>
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide3" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide2" className="btn btn-circle">
-            ❯
-          </a>
+const Banner = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <div className="relative">
+      <Slider {...settings}>
+        <div className="relative">
+          <img
+            src="https://i.ibb.co/wg7HbXX/1.jpg"
+            alt="Image 1"
+            className="object-cover h-80 w-full opacity-75"
+          />
+          <div className=" absolute top-1/2 left-52 transform -translate-x-1/2 -translate-y-1/2 text-black">
+            <h1 className="text-4xl font-bold mb-4 bg-lime-50 rounded-xl p-2">
+              Welcome to <span className="text-purple-700">NestQuest</span>
+            </h1>
+            <p className="text-xl font-medium text-lime-600">
+              Real Estate company to fullfill your Dream
+            </p>
+
+            <input
+              type="text"
+              placeholder="Search....."
+              className="bg-transparent border-b-2 border-purple-700 px-4 py-2 text-white placeholder-purple-900 focus:outline-none"
+            />
+          </div>
         </div>
-      </div>
-      <div id="slide2" className="carousel-item relative w-full">
-        <img
-          src="https://i.ibb.co/KbmdW7z/banner2.jpg"
-          className="w-full object-cover opacity-95"
-        />
-        <div className="absolute inset-0 flex md:items-center justify-center">
-          <h2 className="animate__animated animate__bounceInDown text-5xl text-orange-500 font-bold bg-stone-100 p-2">
-            GET STARTED RIGHT NOW!
-          </h2>
+        <div className="relative">
+          <img
+            src="https://i.ibb.co/KbmdW7z/banner2.jpg"
+            alt="Image 2"
+            className="object-cover h-80 w-full opacity-75"
+          />
+          <div className="absolute top-1/2 left-52 transform -translate-x-1/2 -translate-y-1/2 text-black">
+            <h1 className="text-4xl font-bold mb-4 bg-lime-50 rounded-xl p-2">
+              Welcome to
+              <span className="text-purple-700 "> NestQuest</span>
+            </h1>
+            <p className="text-xl font-medium text-lime-600">
+              Real Estate company to fullfill your Dream
+            </p>
+
+            <input
+              type="text"
+              placeholder="Search....."
+              className="bg-transparent border-b-2 border-purple-700 px-4 py-2 text-white placeholder-purple-900 focus:outline-none"
+            />
+          </div>
         </div>
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide1" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide3" className="btn btn-circle">
-            ❯
-          </a>
+        <div className="relative">
+          <img
+            src="https://i.ibb.co/TqCvWSG/4.jpg"
+            alt="Image 3"
+            className="object-cover h-80 w-full opacity-75"
+          />
+          <div className="absolute top-1/2 left-52 transform -translate-x-1/2 -translate-y-1/2 text-black">
+            <h1 className="text-4xl font-bold mb-4 bg-lime-50 rounded-xl p-2">
+              Welcome to <span className="text-purple-700">NestQuest</span>
+            </h1>
+            <p className="text-xl font-medium text-lime-600 rounded-xl p-2">
+              Real Estate company to fullfill your Dream
+            </p>
+
+            <input
+              type="text"
+              placeholder="Search....."
+              className="bg-transparent border-b-2 border-purple-700 px-4 py-2 text-white placeholder-purple-900 focus:outline-none"
+            />
+          </div>
         </div>
-      </div>
-      <div id="slide3" className="carousel-item relative w-full">
-        <img
-          src="https://i.ibb.co/bFY9yrW/banner3.webp"
-          className="w-full object-cover opacity-95"
-        />
-        <div className="absolute inset-0 flex md:items-center justify-center">
-          <h2 className="animate__animated animate__bounceInDown text-5xl text-orange-500 font-bold bg-stone-100 p-2">
-            Best Houses to Buy
-          </h2>
-        </div>
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide2" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide1" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
+      </Slider>
     </div>
   );
 };
